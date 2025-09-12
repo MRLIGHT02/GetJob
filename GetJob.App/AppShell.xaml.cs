47616a7e00b91c3ba4cdc44a398cd03839b90d3a
MRLIGHT02 <saifali42020@gmail.com>
2025-09-12 18:32:22 +0530

    Added .Maui Project To Solution For View

---
+++ GetJob.App/AppShell.xaml.cs
@@ -0,0 +1,10 @@
+﻿namespace GetJob.App
+{
+    public partial class AppShell : Shell
+    {
+        public AppShell()
+        {
+            InitializeComponent();
+        }
+    }
+}
---
+++ GetJob.App/Views/Pages/HomePage.xaml.cs
@@ -0,0 +1,9 @@
+namespace GetJob.App.Views.Pages;
+
+public partial class HomePage : ContentPage
+{
+	public HomePage()
+	{
+		InitializeComponent();
+	}
+}
